eb484f994adcab9e3bacdfb703b63eeb5084ac1c
zemgalis <maris.zemgalis@gmail.com>
2026-09-09 13:16:03 +0100

    feat(controller): add payment endpoint

--- src/main/java/com/checkout/payment/gateway/controller/PaymentGatewayController.java
+++ src/main/java/com/checkout/payment/gateway/controller/PaymentGatewayController.java
@@ -1,12 +1,16 @@
 package com.checkout.payment.gateway.controller;
 
+import com.checkout.payment.gateway.model.PostPaymentRequest;
 import com.checkout.payment.gateway.model.PostPaymentResponse;
 import com.checkout.payment.gateway.service.PaymentGatewayService;
+import jakarta.validation.Valid;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api")
@@ -21,5 +25,12 @@ public class PaymentGatewayController {
   @GetMapping("/payment/{id}")
   public ResponseEntity<PostPaymentResponse> getPostPaymentEventById(@PathVariable UUID id) {
     return new ResponseEntity<>(paymentGatewayService.getPaymentById(id), HttpStatus.OK);
+  }
+
+  @PostMapping("/payment")
+  public ResponseEntity<PostPaymentResponse> processPayment(@Valid @RequestBody PostPaymentRequest request) {
+    PostPaymentResponse response = paymentGatewayService.processPayment(request);
+
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
 }
